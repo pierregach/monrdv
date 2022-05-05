2f474f6568207a7bb2f9be12c7c9d22d17af8d6e
unknown <gachpierre@gmail.com>
2022-05-05 12:19:09 +0200

    specialite.ts

--- monrdvAngular/src/app/model/Praticien.ts
+++ monrdvAngular/src/app/model/Praticien.ts
@@ -1,3 +1,43 @@
-export class Praticien {
+import { Adresse } from "./Adresse";
+import { Utilisateur } from "./Utilisateur"
 
+export class Praticien extends Utilisateur{
+  matricule: string;
+  telephone: string;
+  photo: string;
+  convention: string;
+  carteVitale: boolean;
+  cb: boolean;
+  cheque: boolean;
+  espece: boolean;
+  dureeCreneau: number;
+
+  constructor(
+    id?: number,
+    nom?: string,
+    prenom?: string,
+    email?: string,
+    motDePasse?: string,
+    adresse?: Adresse,
+    matricule?: string,
+    telephone?: string,
+    photo?: string,
+    convention?: string,
+    carteVitale?: boolean,
+    cb?: boolean,
+    cheque?: boolean,
+    espece?: boolean,
+    dureeCreneau?: number
+  ) {
+    super(id, nom, prenom, email, motDePasse, adresse);
+    this.matricule = matricule;
+    this.telephone = telephone;
+    this.photo = photo;
+    this.convention = convention;
+    this.carteVitale = carteVitale;
+    this.cb = cb;
+    this.cheque = cheque;
+    this.espece = espece;
+    this.dureeCreneau = dureeCreneau;
+  }
 }
